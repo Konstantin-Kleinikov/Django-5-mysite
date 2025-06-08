bbb929d5f824bb571bc6a8c13acc9e3e00b879d3
Констатин Клейников <kkleinikov@yandex.ru>
2025-06-08 21:27:56 +0300

    1.1 Implement Post model and admin configuration for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,48 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        )
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    _max_status_length = max(
+        len(status_value) for status_value, _ in Status.choices
+    )
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='blog_posts',
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=_max_status_length,
+        choices=Status,
+        default=Status.DRAFT
+    )
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title
